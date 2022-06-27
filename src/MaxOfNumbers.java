4d017ffa24be92419ab783bac792afecabb934e9
unknown <https://github.com/Pooja-bhaskaran/Day03.git>
2022-06-27 12:30:15 +0530

    String max added

--- src/MaxOfNumbers.java
+++ src/MaxOfNumbers.java
@@ -1,18 +1,18 @@
 public class MaxOfNumbers<T> {
 
     public static void main(String[] args) {
-        Float obj1 = 737.23F;
-        Float obj2 = 90.2F;
-        Float obj3 = 111.3F;
-        Float max = obj1;
+            String obj1="Apple";
+            String obj2="Peach";
+            String obj3="Banana";
+            String max=obj1;
 
-        if (obj2.compareTo(max) > 0) {
-            max = obj2;
-        }
-        if (obj3.compareTo(max) > 0) {
-            max = obj3;
-        }
-        System.out.println("Max of three Floating number is: " + max);
+            if (obj2.compareTo(max) > 0) {
+                max = obj2;
+            }
+            if (obj3.compareTo(max) > 0) {
+                max = obj3;
+            }
+            System.out.println("Max of three Strings is: " + max);
     }
 }
 
